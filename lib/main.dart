1907c24a6190afa100d61f08aafd1591706583f3
Ziad Ehab <123027492+Ziadehab1@users.noreply.github.com>
2026-04-25 03:14:43 +0300

    Update dependencies and configuration
    - Set Kotlin incremental compilation to false in gradle.properties.
    - Refactor main.dart for cleaner formatting of MaterialApp.
    - Downgrade audioplayers and related packages in pubspec.lock to resolve compatibility issues.
    - Update audioplayers version in pubspec.yaml to 5.2.1 and video_player to 2.8.6.

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ class BrainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, 
-    home: SplashScreen());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
   }
 }
